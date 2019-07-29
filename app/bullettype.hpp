f725f3c3000e004f960860bb635cfe90c7610a2f
TadekBiela <tadek.biela@gmail.com>
2019-07-29 21:45:56 +0200

    Added BulletType enum

---
+++ app/bullettype.hpp
@@ -0,0 +1,13 @@
+#ifndef BULLETTYPE_H
+#define BULLETTYPE_H
+
+enum bullet_type
+{
+    playerDefaultBullet = 0,
+    playerRedBullet,
+    playerYellowBullet,
+    playerBlueBullet,
+    enemyBullet
+};
+
+#endif //BULLETTYPE_H
