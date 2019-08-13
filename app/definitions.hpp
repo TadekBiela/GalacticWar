7c7ec0c5b02e49afa142c21cda70d81849363d12
TadekBiela <tadek.biela@gmail.com>
2019-08-13 20:12:39 +0200

    Added consts to definitions variables

--- app/definitions.hpp
+++ app/definitions.hpp
@@ -3,9 +3,10 @@
 
 namespace def
 {
-    int    moveVectorLength       = 10;
-    double pi                     = 3.14159265;
-    double radianConversionFactor = pi / 180;
+    const int    animationFrameDuration = 5;
+    const int    moveVectorLength       = 10;
+    const double pi                     = 3.14159265;
+    const double radianConversionFactor = pi / 180;
 }
 
 #endif // DEFINITIONS_HPP
