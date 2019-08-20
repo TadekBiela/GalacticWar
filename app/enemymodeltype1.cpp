c222c29328202848c8f97c37c3b45521acca62ca
TadekBiela <tadek.biela@gmail.com>
2019-08-20 12:27:41 +0200

    Added EnemyModelType1 fire() func implementation

--- app/enemymodeltype1.cpp
+++ app/enemymodeltype1.cpp
@@ -1,4 +1,7 @@
 #include "enemymodeltype1.hpp"
+#include "bulletmodel.hpp"
+#include "definitions.hpp"
+#include <QGraphicsScene>
 
 EnemyModelType1::EnemyModelType1(QPointF position) :
                                  EnemyModel(1,
@@ -18,7 +21,12 @@ EnemyModelType1::~EnemyModelType1()
 
 void EnemyModelType1::fire()
 {
-
+    BulletModel* bullet = new BulletModel(bullet_type::enemyBullet,
+                                          pos(),
+                                          m_damage,
+                                          def::down,
+                                          def::defaultBulletSpeed);
+    QGraphicsItem::scene()->addItem(bullet);
 }
 
 void EnemyModelType1::move()
