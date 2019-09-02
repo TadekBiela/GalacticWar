a59ea3517281595a07ac8603945ce1806c8591a0
TadekBiela <tadek.biela@gmail.com>
2019-09-02 21:52:00 +0200

    PlayerModel constructor implementation

--- app/playermodel.cpp
+++ app/playermodel.cpp
@@ -1,8 +1,14 @@
 #include "playermodel.hpp"
+#include "definitions.hpp"
+#include "firefunctions.hpp"
 
-PlayerModel::PlayerModel()
+PlayerModel::PlayerModel() :
+                         m_moveDirection(def::halfSceneWight, def::halfSceneHeight),
+                         m_weapon(weapon_type::defaultWeapon),
+                         m_fireFuncPtr(&defaultFireFunc)
 {
-
+    connect(&m_moveTimer, SIGNAL(timeout()), this, SLOT(move()));
+    connect(&m_fireTimer, SIGNAL(timeout()), this, SLOT(fire()));
 }
 
 PlayerModel::~PlayerModel()
