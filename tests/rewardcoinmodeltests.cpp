efeba939ad8c06f2099da29badd9e55cde27d110
TadekBiela <tadek.biela@gmail.com>
2019-09-09 11:35:40 +0200

    RewardCoinModel animation() func UTs

--- tests/rewardcoinmodeltests.cpp
+++ tests/rewardcoinmodeltests.cpp
@@ -15,6 +15,7 @@ public:
     int           getAnimationFrameIdx() const { return m_animationFrameIdx; }
     const QTimer& getAnimationTimer()    const { return m_animationTimer; }
     const QTimer& getDestroyTimer()      const { return m_destroyTimer; }
+    void          setAnimationFrameIdx(int idx) { m_animationFrameIdx = idx; }
 };
 
 class RewardCoinModelTestsClass : public testing::Test
@@ -39,4 +40,25 @@ TEST_F(RewardCoinModelTestsClass, RewardCoinModelConstructor_CheckBuildModelCorr
     EXPECT_FLOAT_EQ(resultDestroyTimer.interval(), def::defaultRewardDestroyTimeDelay);
     EXPECT_NEAR(resultAnimTime,                    def::animationFrameDuration, 1);
     EXPECT_NEAR(resultDestroyTime,                 def::defaultRewardDestroyTimeDelay, 1);
+}
+
+TEST_F(RewardCoinModelTestsClass, Animation_AnimationFrameIdxIs0CheckIfWillBeIncreased_IsOne)
+{
+    RewardCoinModelTest rewardCoinModel(coin_type::bronze);
+
+    rewardCoinModel.animation();
+    int resultAnimFrameIdx = rewardCoinModel.getAnimationFrameIdx();
+
+    EXPECT_EQ(resultAnimFrameIdx, 1);
+}
+
+TEST_F(RewardCoinModelTestsClass, Animation_AnimationFrameIdxIsMaxCheckIfWillBeResetTo0_IsZero)
+{
+    RewardCoinModelTest rewardCoinModel(coin_type::bronze);
+    rewardCoinModel.setAnimationFrameIdx(def::maxFrameIdx);
+
+    rewardCoinModel.animation();
+    int resultAnimFrameIdx = rewardCoinModel.getAnimationFrameIdx();
+
+    EXPECT_EQ(resultAnimFrameIdx, 0);
 }
