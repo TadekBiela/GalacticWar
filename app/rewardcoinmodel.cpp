f0d069d2a98f5c2f224c6f0c4f41a32c19b7beb6
TadekBiela <tadek.biela@gmail.com>
2019-09-09 11:17:21 +0200

    RewardCoinModel class without implementation

---
+++ app/rewardcoinmodel.cpp
@@ -0,0 +1,17 @@
+#include "rewardcoinmodel.hpp"
+
+RewardCoinModel::RewardCoinModel(coin_type type) :
+                                 m_type(type)
+{
+
+}
+
+void RewardCoinModel::collected()
+{
+
+}
+
+void RewardCoinModel::animation()
+{
+
+}
---
+++ app/rewardcoinmodel.hpp
@@ -0,0 +1,23 @@
+#ifndef REWARDCOINMODEL_HPP
+#define REWARDCOINMODEL_HPP
+
+#include "rewardmodel.hpp"
+#include "rewardtypes.hpp"
+
+class RewardCoinModel : public RewardModel
+{
+public:
+    RewardCoinModel(coin_type type);
+    void collected();
+
+signals:
+    void collected(coin_type type);
+
+public slots:
+    void animation();
+
+protected:
+    coin_type m_type
+};
+
+#endif // REWARDCOINMODEL_HPP
