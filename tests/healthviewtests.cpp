2fba31393fe444cf012eb77ca00ec7b4da956ef6
TadekBiela <tadek.biela@gmail.com>
2019-09-11 15:10:06 +0200

    HealthView updateHealthView() func UTs

---
+++ tests/healthviewtests.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include "../app/healthview.hpp"
+
+class HealthViewTests : public HealthView
+{
+public:
+    const QProgressBar& getHealthBar() { return m_healthBar; }
+};
+
+class HealthViewTestsClass : public testing::Test
+{
+};
+
+TEST_F(HealthViewTestsClass, UpdateHealthView_UpdateWithValue0_IsZero)
+{
+    HealthViewTests healthView;
+
+    healthView.updateHealthView(0);
+    const QProgressBar& resultHealthBar = healthView.getHealthBar();
+
+    EXPECT_EQ(resultHealthBar.value(), 0);
+}
+
+TEST_F(HealthViewTestsClass, UpdateHealthView_UpdateWithValue30_IsEqual)
+{
+    HealthViewTests healthView;
+
+    healthView.updateHealthView(30);
+    const QProgressBar& resultHealthBar = healthView.getHealthBar();
+
+    EXPECT_EQ(resultHealthBar.value(), 10);
+}
+
+TEST_F(HealthViewTestsClass, UpdateHealthView_UpdateWithMaxValue_IsEqual)
+{
+    HealthViewTests healthView;
+
+    healthView.updateHealthView(100);
+    const QProgressBar& resultHealthBar = healthView.getHealthBar();
+
+    EXPECT_EQ(resultHealthBar.value(), 100);
+}
+
+TEST_F(HealthViewTestsClass, UpdateHealthView_UpdateWithMoreThanMaxValueShouldntIncreaseMoreThanMax_IsEqual)
+{
+    HealthViewTests healthView;
+
+    healthView.updateHealthView(120);
+    const QProgressBar& resultHealthBar = healthView.getHealthBar();
+
+    EXPECT_EQ(resultHealthBar.value(), 100);
+}
