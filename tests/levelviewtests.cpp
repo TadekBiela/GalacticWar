f897b363c55517e7345b6eae68fb1d41bfc0c5e9
TadekBiela <tadek.biela@gmail.com>
2019-09-12 10:06:34 +0200

    LevelView update() func UTs

---
+++ tests/levelviewtests.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "../app/levelview.hpp"
+#include "../app/definitions.hpp"
+#include <QLabel>
+#include <QString>
+
+class LevelViewTests : public LevelView
+{
+public:
+    const QLabel& getLevelLabel() { return m_levelLabel; }
+};
+
+class LevelViewTestsClass : public testing::Test
+{
+};
+
+TEST_F(LevelViewTestsClass, Update_UpdateWithValue1_IsOne)
+{
+    LevelViewTests levelView;
+
+    levelView.update(1);
+    const QLabel& resultLevelLabel = levelView.getLevelLabel();
+
+    EXPECT_EQ(resultLevelLabel.text(), QString(1));
+}
+
+TEST_F(LevelViewTestsClass, Update_UpdateWithValueMax_IsEqual)
+{
+    LevelViewTests levelView;
+
+    levelView.update(def::maxNumOfLevel);
+    const QLabel& resultLevelLabel = levelView.getLevelLabel();
+
+    EXPECT_EQ(resultLevelLabel.text(), QString(def::maxNumOfLevel));
+}
