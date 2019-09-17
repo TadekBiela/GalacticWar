86e334ad7b7397ecc79118322ff350248cc54cf7
TadekBiela <tadek.biela@gmail.com>
2019-09-17 21:42:56 +0200

    GeneralView all funcs implementations

--- app/generalview.cpp
+++ app/generalview.cpp
@@ -2,16 +2,16 @@
 #include "definitions.hpp"
 
 GeneralView::GeneralView() :
-                         m_scene(0, 0, def::sceneWight, def::sceneHeight, this),
-                         m_title("GALACTIC WAR", this),
-                         m_author("Created by TED", this),
-                         m_startButton("MENU", this),
-                         m_highScoreButton("HIGH SCORE", this),
-                         m_quitButton("QUIT", this),
-                         m_backToMenuButton("BACK", this),
-                         m_saveAfterGameOver("SAVE", this)
+                         m_scene(0, 0, def::sceneWight, def::sceneHeight),
+                         m_title("GALACTIC WAR"),
+                         m_author("Created by TED"),
+                         m_startButton("MENU"),
+                         m_highScoreButton("HIGH SCORE"),
+                         m_quitButton("QUIT"),
+                         m_backToMenuButton("BACK"),
+                         m_saveAfterGameOver("SAVE")
 {
-
+    menu();
 }
 
 GeneralView::~GeneralView()
@@ -21,27 +21,67 @@ GeneralView::~GeneralView()
 
 void GeneralView::menu()
 {
-
+    m_title.setVisible(true);
+    m_author.setVisible(true);
+    m_pause.setVisible(false);
+    m_startButton.setVisible(true);
+    m_highScoreButton.setVisible(true);
+    m_quitButton.setVisible(true);
+    m_backToMenuButton.setVisible(false);
+    m_saveAfterGameOver.setVisible(false);
+    m_highScoreList.setVisible(false);
 }
 
 void GeneralView::startGame()
 {
-
+    m_title.setVisible(false);
+    m_author.setVisible(false);
+    m_pause.setVisible(false);
+    m_startButton.setVisible(false);
+    m_highScoreButton.setVisible(false);
+    m_quitButton.setVisible(false);
+    m_backToMenuButton.setVisible(false);
+    m_saveAfterGameOver.setVisible(false);
+    m_highScoreList.setVisible(false);
 }
 
 void GeneralView::pause()
 {
-
+    m_title.setVisible(false);
+    m_author.setVisible(false);
+    m_pause.setVisible(true);
+    m_startButton.setVisible(false);
+    m_highScoreButton.setVisible(false);
+    m_quitButton.setVisible(false);
+    m_backToMenuButton.setVisible(false);
+    m_saveAfterGameOver.setVisible(false);
+    m_highScoreList.setVisible(false);
 }
 
 void GeneralView::gameOver()
 {
-
+    m_title.setVisible(false);
+    m_author.setVisible(true);
+    m_pause.setVisible(false);
+    m_startButton.setVisible(false);
+    m_highScoreButton.setVisible(false);
+    m_quitButton.setVisible(false);
+    m_backToMenuButton.setVisible(true);
+    m_saveAfterGameOver.setVisible(true);
+    m_highScoreList.setVisible(false);
 }
 
 void GeneralView::highScore()
 {
-
+    m_title.setVisible(true);
+    m_author.setVisible(true);
+    m_pause.setVisible(false);
+    m_startButton.setVisible(false);
+    m_highScoreButton.setVisible(false);
+    m_quitButton.setVisible(false);
+    m_backToMenuButton.setVisible(true);
+    m_saveAfterGameOver.setVisible(false);
+    m_highScoreList.setVisible(true);
 }
 
 void GeneralView::addGameObject(QGraphicsItem* newObject)
