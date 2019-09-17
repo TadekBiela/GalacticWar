e618d088beb19cff0d96b8a14816ebaf6ac4cde1
TadekBiela <tadek.biela@gmail.com>
2019-09-17 22:47:17 +0200

    EnemyController destroyed() func UT

--- tests/enemycontrollertests.cpp
+++ tests/enemycontrollertests.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
+#include "utdefinitions.hpp"
 #include "../app/enemycontroller.hpp"
+#include <QSignalSpy>
+#include <QPointF>
 
 class EnemyControllerTest : public EnemyController
 {
@@ -30,4 +33,20 @@ TEST_F(EnemyControllerTestsClass, ChangeEnemyConfiguration_NewConfigShouldReplac
     EXPECT_EQ(resultCurrConfig.proportionOfEnemyType4, 25);
     EXPECT_EQ(resultCurrConfig.proportionOfEnemyType5, 30);
     EXPECT_EQ(resultCurrConfig.proportionOfEnemyType6,  0);
+}
+
+TEST_F(EnemyControllerTestsClass, Destroyed_ShouldEmitEnemyDestroyedSignalWithSameParameters_IsEqual)
+{
+    GeneralView*        view = new GeneralView;
+    EnemyControllerTest enemyController(view);
+    QSignalSpy          signalDestroyed(&enemyController, &EnemyControllerTest::enemyDestroyed);
+    signalDestroyed.wait(utdef::minSignalTimeDelay);
+
+    enemyController.destroyed(QPointF(40, 130), 4);
+    int             resultSignalDestroyedCount = signalDestroyed.count();
+    QList<QVariant> resultSignalDestroyed      = signalDestroyed.takeFirst();
+
+    EXPECT_EQ(resultSignalDestroyedCount,             1);
+    EXPECT_EQ(resultSignalDestroyed.at(0).toPointF(), QPointF(40,130));
+    EXPECT_EQ(resultSignalDestroyed.at(1).toInt(),    4);
 }
