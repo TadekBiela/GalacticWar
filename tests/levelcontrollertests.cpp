4980b022cf72dc59ffdd29b1ba2179f0111ad34e
TadekBiela <tadek.biela@gmail.com>
2019-09-27 14:39:37 +0200

    LevelController changeEnemyConfig() func UT

--- tests/levelcontrollertests.cpp
+++ tests/levelcontrollertests.cpp
@@ -5,6 +5,8 @@
 #include "../app/levelview.hpp"
 #include <QSignalSpy>
 
+Q_DECLARE_METATYPE(EnemyConfiguration)
+
 class LevelControllerTest : public LevelController
 {
 public:
@@ -27,9 +29,34 @@ TEST_F(LevelControllerTestsClass, NextLevel_CheckIfWillSendNextLevelInModelSigna
     signalNext.wait(utdef::minSignalTimeDelay);
 
     levelController.nextLevel();
-    int resultSignalAddCount = signalNext.count();
+    int resultSignalNextLevelCount = signalNext.count();
 
-    EXPECT_EQ(resultSignalAddCount, 1);
+    EXPECT_EQ(resultSignalNextLevelCount, 1);
+    delete model;
+    delete view;
+}
+
+TEST_F(LevelControllerTestsClass, ChangeEnemyConfig_CheckIfWillSendChangeEnemyConfigurationSignalOneTime_IsEqual)
+{
+    qRegisterMetaType<EnemyConfiguration>();
+    EnemyConfiguration inputEnemyConfig = { 10, 50, 0, 25, 15, 0 };
+    LevelModel* model = new LevelModel;
+    LevelView*  view  = new LevelView;
+    LevelControllerTest levelController(model, view);
+    QSignalSpy signalChange(&levelController, &LevelControllerTest::changeEnemyConfiguration);
+    signalChange.wait(utdef::minSignalTimeDelay);
+
+    levelController.changeEnemyConfig(inputEnemyConfig);
+    int                resultSignalAddCount = signalChange.count();
+    EnemyConfiguration resultEnemyConfig    = qvariant_cast<EnemyConfiguration>(signalChange.takeFirst().at(0));
+
+    EXPECT_EQ(resultSignalAddCount,                     1);
+    EXPECT_EQ(resultEnemyConfig.proportionOfEnemyType1, inputEnemyConfig.proportionOfEnemyType1);
+    EXPECT_EQ(resultEnemyConfig.proportionOfEnemyType2, inputEnemyConfig.proportionOfEnemyType2);
+    EXPECT_EQ(resultEnemyConfig.proportionOfEnemyType3, inputEnemyConfig.proportionOfEnemyType3);
+    EXPECT_EQ(resultEnemyConfig.proportionOfEnemyType4, inputEnemyConfig.proportionOfEnemyType4);
+    EXPECT_EQ(resultEnemyConfig.proportionOfEnemyType5, inputEnemyConfig.proportionOfEnemyType5);
+    EXPECT_EQ(resultEnemyConfig.proportionOfEnemyType6, inputEnemyConfig.proportionOfEnemyType6);
     delete model;
     delete view;
 }
