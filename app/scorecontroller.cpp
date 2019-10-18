68c9e5793be3049e4374e8227562f8a4a8fceedf
TadekBiela <tadek.biela@gmail.com>
2019-10-18 21:20:36 +0200

    ScoreController class without implementation

---
+++ app/scorecontroller.cpp
@@ -0,0 +1,27 @@
+#include "scorecontroller.hpp"
+
+ScoreController::ScoreController(ScoreModel *model,
+                                 ScoreView  *view)
+{
+
+}
+
+ScoreController::~ScoreController()
+{
+
+}
+
+void ScoreController::getScore()
+{
+
+}
+
+void ScoreController::addScorePoints(coin_type coin)
+{
+
+}
+
+void ScoreController::maxPerLevelAchived()
+{
+
+}
---
+++ app/scorecontroller.hpp
@@ -0,0 +1,28 @@
+#ifndef SCORECONTROLLER_HPP
+#define SCORECONTROLLER_HPP
+
+#include "scoremodel.hpp"
+#include "scoreview.hpp"
+#include "rewardtypes.hpp"
+#include <QObject>
+
+class ScoreController : public QObject
+{
+    Q_OBJECT
+public:
+    ScoreController(ScoreModel* model,
+                    ScoreView*  view);
+    virtual ~ScoreController();
+
+signals:
+    void updateScore(int score);
+    void addPoints(int points);
+    void maxScorePerLevelAchived();
+
+public slots:
+    void getScore();
+    void addScorePoints(coin_type coin);
+    void maxPerLevelAchived();
+};
+
+#endif // SCORECONTROLLER_HPP
