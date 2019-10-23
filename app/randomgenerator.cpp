22fef777d9f202dccc64ec2f5642eb7e7314e9aa
TadekBiela <tadek.biela@gmail.com>
2019-10-23 16:30:03 +0200

    Added RandomGenerator class

---
+++ app/randomgenerator.cpp
@@ -0,0 +1,16 @@
+#include "randomgenerator.hpp"
+
+RandomGenerator::RandomGenerator()
+{
+
+}
+
+RandomGenerator::~RandomGenerator()
+{
+
+}
+
+int RandomGenerator::bounded(int lowest, int highest)
+{
+    return generator.bounded(lowest, highest);
+}
---
+++ app/randomgenerator.hpp
@@ -0,0 +1,18 @@
+#ifndef RANDOMGENERATOR_HPP
+#define RANDOMGENERATOR_HPP
+
+#include "irandomgenerator.hpp"
+#include <QRandomGenerator>
+
+class RandomGenerator : public IRandomGenerator
+{
+public:
+    RandomGenerator();
+    virtual ~RandomGenerator();
+    int bounded(int lowest, int highest);
+
+protected:
+    QRandomGenerator generator;
+};
+
+#endif // RANDOMGENERATOR_HPP
