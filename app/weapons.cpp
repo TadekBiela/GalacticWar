54b0bfcfc56d39224f751f29b7c03e410b27cd61
TadekBiela <tadek.biela@gmail.com>
2020-01-17 21:20:04 +0100

    Changed fire rate in all weapons

--- app/weapons.cpp
+++ app/weapons.cpp
@@ -3,21 +3,21 @@
 weapon defaultWeapon = { weapon_type::defWeapon, 10, &defaultFireFunc, 300 };
 
 weapon weapons[def::maxWeaponTypes * def::maxWeaponLevel] = {
-    { weapon_type::redWeapon, 20, &redLevel1FireFunc, 20 },
-    { weapon_type::redWeapon, 20, &redLevel2FireFunc, 18 },
-    { weapon_type::redWeapon, 40, &redLevel3FireFunc, 16 },
-    { weapon_type::redWeapon, 60, &redLevel4FireFunc, 14 },
-    { weapon_type::redWeapon, 80, &redLevel5FireFunc, 12 },
+    { weapon_type::redWeapon, 20, &redLevel1FireFunc, 200 },
+    { weapon_type::redWeapon, 20, &redLevel2FireFunc, 180 },
+    { weapon_type::redWeapon, 40, &redLevel3FireFunc, 160 },
+    { weapon_type::redWeapon, 60, &redLevel4FireFunc, 140 },
+    { weapon_type::redWeapon, 80, &redLevel5FireFunc, 120 },
 
-    { weapon_type::yellowWeapon,  30, &yellowLevel1FireFunc, 30 },
-    { weapon_type::yellowWeapon,  40, &yellowLevel2FireFunc, 26 },
-    { weapon_type::yellowWeapon,  40, &yellowLevel3FireFunc, 24 },
-    { weapon_type::yellowWeapon,  80, &yellowLevel4FireFunc, 20 },
-    { weapon_type::yellowWeapon, 120, &yellowLevel5FireFunc, 18 },
+    { weapon_type::yellowWeapon,  30, &yellowLevel1FireFunc, 300 },
+    { weapon_type::yellowWeapon,  40, &yellowLevel2FireFunc, 260 },
+    { weapon_type::yellowWeapon,  40, &yellowLevel3FireFunc, 240 },
+    { weapon_type::yellowWeapon,  80, &yellowLevel4FireFunc, 200 },
+    { weapon_type::yellowWeapon, 120, &yellowLevel5FireFunc, 180 },
 
-    { weapon_type::blueWeapon, 12, &blueLevel1FireFunc, 14 },
-    { weapon_type::blueWeapon, 12, &blueLevel2FireFunc, 12 },
-    { weapon_type::blueWeapon, 15, &blueLevel3FireFunc, 10 },
-    { weapon_type::blueWeapon, 20, &blueLevel4FireFunc,  8 },
-    { weapon_type::blueWeapon, 30, &blueLevel5FireFunc,  6 }
+    { weapon_type::blueWeapon, 12, &blueLevel1FireFunc, 140 },
+    { weapon_type::blueWeapon, 12, &blueLevel2FireFunc, 120 },
+    { weapon_type::blueWeapon, 15, &blueLevel3FireFunc, 100 },
+    { weapon_type::blueWeapon, 20, &blueLevel4FireFunc,  80 },
+    { weapon_type::blueWeapon, 30, &blueLevel5FireFunc,  60 }
                                           };
