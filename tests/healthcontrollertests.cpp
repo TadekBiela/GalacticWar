1cd054ee8744c363bb3d621c35548483207e255b
TadekBiela <tadek.biela@gmail.com>
2020-07-22 00:29:46 +0200

    Names corrections in HealthController UTs

--- tests/healthcontrollertests.cpp
+++ tests/healthcontrollertests.cpp
@@ -19,47 +19,45 @@ public:
     MOCK_METHOD(Health_Status, subtract, (int), ());
 };
 
-class HealthControllerTests : public HealthController
+class HealthControllerTest : public HealthController
 {
 public:
-    HealthControllerTests(QWidget*         displayWidget,
-                          HealthModelMock* modelMock,
-                          BarViewStub*     viewMock)
+    HealthControllerTest(QWidget*         displayWidget,
+                         HealthModelMock* modelMock,
+                         BarViewStub*     viewMock)
         : HealthController(displayWidget)
     {
         m_model = modelMock;
         m_view  = viewMock;
     }
-    virtual ~HealthControllerTests()
+    virtual ~HealthControllerTest()
     {
         m_model = nullptr;
         m_view  = nullptr;
     }
 };
 
-class HealthControllerlTestsClass : public testing::Test
+class HealthControllerTestsClass : public testing::Test
 {
 public:
     void SetUp()
     {
         g_imageStorage = new ImageStorageStub;
-        g_soundStorage = new SoundStorageStub;
     }
     void TearDown()
     {
         delete g_imageStorage;
-        delete g_soundStorage;
     }
 };
 
-TEST_F(HealthControllerlTestsClass, SubtractHealthPoints_currentHealthIsMaxSubtract10Points_ShouldNotSendSignalNoHealth)
+TEST_F(HealthControllerTestsClass, SubtractHealthPoints_currentHealthIsMaxSubtract10Points_ShouldNotSendSignalNoHealth)
 {
-    QWidget               displayWidget;
-    HealthModelMock       modelMock;
-    BarViewStub           viewMock(&displayWidget);
-    HealthControllerTests controller(&displayWidget, &modelMock, &viewMock);
+    QWidget              displayWidget;
+    HealthModelMock      modelMock;
+    BarViewStub          viewMock(&displayWidget);
+    HealthControllerTest controller(&displayWidget, &modelMock, &viewMock);
     ON_CALL(modelMock, subtract(_)).WillByDefault(Return(Health_Status_Still_Is));
-    QSignalSpy signalNoHealth(&controller, &HealthControllerTests::noHealth);
+    QSignalSpy signalNoHealth(&controller, &HealthControllerTest::noHealth);
     signalNoHealth.wait(utdef::minSignalTimeDelay);
 
     controller.subtractHealthPoints(10);
@@ -68,14 +66,14 @@ TEST_F(HealthControllerlTestsClass, SubtractHealthPoints_currentHealthIsMaxSubtr
     EXPECT_EQ(0, resultSignalNoHealth);
 }
 
-TEST_F(HealthControllerlTestsClass, SubtractHealthPoints_currentHealthIsMaxSubtractMaxPoints_ShouldSendSignalNoHealth)
+TEST_F(HealthControllerTestsClass, SubtractHealthPoints_currentHealthIsMaxSubtractMaxPoints_ShouldSendSignalNoHealth)
 {
-    QWidget               displayWidget;
-    HealthModelMock       modelMock;
-    BarViewStub           viewMock(&displayWidget);
-    HealthControllerTests controller(&displayWidget, &modelMock, &viewMock);
+    QWidget              displayWidget;
+    HealthModelMock      modelMock;
+    BarViewStub          viewMock(&displayWidget);
+    HealthControllerTest controller(&displayWidget, &modelMock, &viewMock);
     ON_CALL(modelMock, subtract(def::maxPlayerHealth)).WillByDefault(Return(Health_Status_No_More));
-    QSignalSpy signalNoHealth(&controller, &HealthControllerTests::noHealth);
+    QSignalSpy signalNoHealth(&controller, &HealthControllerTest::noHealth);
     signalNoHealth.wait(utdef::minSignalTimeDelay);
 
     controller.subtractHealthPoints(def::maxPlayerHealth);
