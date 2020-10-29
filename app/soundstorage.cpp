fa5d78adb263c47998851404bf844a8165e3d3c3
TadekBiela <tadek.biela@gmail.com>
2020-10-29 15:58:14 +0100

    Added segmentation fault protection when missing sound

--- app/soundstorage.cpp
+++ app/soundstorage.cpp
@@ -1,32 +1,52 @@
 #include "soundstorage.hpp"
 #include <QCoreApplication>
+#include <QDebug>
 #include <QDir>
 #include <QStringList>
 #include <algorithm>
 
 SoundStorage::SoundStorage()
 {
+    m_soundStorage.insert("default", new QSoundEffect());
+
     QDir directory(QCoreApplication::applicationDirPath() + "/sounds/");
-    QStringList sounds = directory.entryList(QStringList() << "*.wav" << "*.WAV", QDir::Files);
-    std::for_each(sounds.begin(), sounds.end(), [this](QString filename)
-    {
-        QString name = filename;
-        name.remove(name.size() - 4, 4);
-        QSoundEffect* sound = new QSoundEffect;
-        sound->setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/sounds/" + filename));
-        this->m_soundStorage.insert(name, sound);
-    });
+    QStringList sounds = directory.entryList(
+        QStringList() << "*.wav" << "*.WAV",
+        QDir::Files
+    );
+    std::for_each(
+        sounds.begin(),
+        sounds.end(),
+        [this](QString filename) {
+            QString name = filename;
+            name.remove(name.size() - 4, 4);
+            QSoundEffect* sound = new QSoundEffect();
+            sound->setSource(
+                QUrl::fromLocalFile(
+                    QCoreApplication::applicationDirPath() + "/sounds/" + filename
+                )
+            );
+            m_soundStorage.insert(name, sound);
+        }
+    );
 }
 
-SoundStorage::~SoundStorage()
-{
-    std::for_each(m_soundStorage.cbegin(), m_soundStorage.cend(), [](QSoundEffect* sound)
-    {
-        delete sound;
-    });
+SoundStorage::~SoundStorage() {
+    std::for_each(
+        m_soundStorage.cbegin(),
+        m_soundStorage.cend(),
+        [](QSoundEffect* sound) {
+            delete sound;
+        }
+    );
 }
 
-QSoundEffect* SoundStorage::getSound(QString name)
-{
-    return *m_soundStorage.find(name);
+QSoundEffect* SoundStorage::getSound(QString name) {
+    auto it = m_soundStorage.find(name);
+    if(m_soundStorage.end() == it) {
+        qDebug() << "Missing sound: " << name;
+        return *m_soundStorage.find("default");
+    }
+
+    return it.value();
 }
