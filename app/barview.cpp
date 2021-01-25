9eb691ae8c346bf1b209a5f424c9d405c39e0e41
TadekBiela <tadek.biela@gmail.com>
2021-01-26 00:06:34 +0100

    Added copyright note to all source files

--- app/barview.cpp
+++ app/barview.cpp
@@ -1,3 +1,20 @@
+
+/************************************************************************************
+** Copyright (C) 2018-2020 Tadeusz Biela (TED)
+** Contact: https://www.linkedin.com/in/tadeuszbiela/
+**
+** This file is part of Galactic War 2D Space Shooter Game.
+**
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU General Public License version 3.
+** The licenses are as published by the Free Software Foundation
+** and appearing in the file LICENSE included in the packaging of this file.
+** Please review the following information to ensure the GNU General Public License
+** requirements will be met:  https://www.gnu.org/licenses/gpl-3.0.html.
+**
+*************************************************************************************/
+
+
 #include "barview.hpp"
 #include "definitions.hpp"
 
